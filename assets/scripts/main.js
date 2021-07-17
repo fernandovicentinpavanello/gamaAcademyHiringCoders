c3dbf072e7137494d2c6b87140621f64603fca4f
Luiz Vicentin <luizpavanello@gmail.com>
2021-07-17 15:58:58 -0300

    Added almost of HTML Front Page

---
+++ assets/scripts/main.js
@@ -0,0 +1,25 @@
+const hamburguer = document.querySelector('hamburguer');
+const navList = document.querySelector('.nav-list');
+
+if (hamburguer) {
+  hamburguer.addEventListener('click', () => {
+    navList.classList.toggle('open');
+  });
+}
+
+// Popup
+
+const popup = document.querySelector('.popup');
+const closePopup = document.querySelector('popup-close');
+
+if (popup) {
+  closePopup.addEventListener('click', () => {
+    popup.classList.add('hide-popup');
+  });
+
+  window.addEventListener('load', () => {
+    setTimeout(() => {
+      popup.classList.remove('hide-popup');
+    }, 1000);
+  });
+}
